91250823539a17c01ce4f9bedbde65a64c1b5576
Davis Mosenkovs <davikovs@gmail.com>
2022-07-22 21:50:28 +0300

    Test time on DoH servers

--- tests/test_connectivity_check_servers.py
+++ tests/test_connectivity_check_servers.py
@@ -1,5 +1,6 @@
 import time
 import unittest
+from datetime import datetime
 
 import pytest
 
@@ -21,6 +22,7 @@ class ConnectivityCheckServersTest(unittest.TestCase):
         for url in mopidy_waitforinternet.check_urls:
             resp = requests.get(url, timeout=2, allow_redirects=False)
             self.assertEqual(resp.status_code, 200)
+            self.assertLess(abs((datetime.utcnow() - datetime.strptime(resp.headers['Date'], '%a, %d %b %Y %H:%M:%S GMT')).total_seconds()), 3)
         t_stop = time.monotonic()
 
         self.assertLess(t_stop - t_start, len(mopidy_waitforinternet.check_urls))
